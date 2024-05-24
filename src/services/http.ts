553195d0d1981c6f5ad83d084c47b66a8399168f
gsemir0418 <845217811@qq.com>
2024-05-24 15:25:40 +0800

    fix: axois baseURL origin

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,19 +1,19 @@
-import axios from "axios";
+import axios from 'axios'
 
 const http = axios.create({
   // baseURL: "http://localhost:8080/api/v1",
-  baseURL: "/dev/api/v1",
+  baseURL: import.meta.env.VITE_ENV === 'development' ? import.meta.env.VITE_API_BASE_URL : `${window.location.origin}/api/v1`,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 })
 
 http.interceptors.response.use(
   response => response,
-  error => {
+  (error) => {
     alert(error)
     return Promise.reject(error)
-  }
+  },
 )
 
 export default http
