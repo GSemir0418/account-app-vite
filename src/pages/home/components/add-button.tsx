6c0a36676345ea51d8bf95fbb87f44ef4264d966
gsemir0418 <845217811@qq.com>
2024-06-14 19:36:25 +0800

    ui: add button & money format

--- src/pages/home/components/add-button.tsx
+++ src/pages/home/components/add-button.tsx
@@ -3,5 +3,5 @@ import { Link } from 'react-router-dom'
 
 interface Props { }
 export const AddButton: React.FC<Props> = () => {
-  return <span className='flex justify-center items-center w-14 h-14 border-solid shadow-2xl rounded-full fixed left-1 bottom-20 bg-teal-200 text-3xl'><Link to="/items/new">+</Link></span>
+  return <span className='flex justify-center items-center w-14 h-14 border-solid shadow-2xl rounded-full fixed left-1 bottom-20 bg-teal-200 text-zinc-400 text-3xl'><Link to="/items/new">+</Link></span>
 }
--- src/pages/home/components/tag-card.tsx
+++ src/pages/home/components/tag-card.tsx
@@ -10,7 +10,7 @@ export const TagCard: React.FC<Props> = ({
   return (
     <div className="max-h-30 w-[30vw] relative flex flex-col items-start p-2 rounded-md shadow-lg bg-neutral-100">
       <span className='text-md text-neutral-500 font-bold truncate max-w-full'><span>{sign}</span>{name}</span>
-      <span className='text-sm text-neutral-400 pt-2'>{summary}</span>
+      <span className='text-sm text-neutral-400 pt-2'>{summary / 1000}</span>
     </div>
   )
 }
