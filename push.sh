67373dd6d9edd1e2225884980abcc92085242b13
gsemir0418 <845217811@qq.com>
2024-06-18 22:48:28 +0800

    chore: add multi-platform supports via docker buildx

--- push.sh
+++ push.sh
@@ -7,6 +7,8 @@ if [ -z "$VERSION" ]; then
     exit 1
 fi
 
-docker build -t account-app-frontend:$VERSION .
-docker tag account-app-frontend:$VERSION gsemir/account-app-frontend:$VERSION
-docker push gsemir/account-app-frontend:$VERSION
+# docker build -t account-app-frontend:$VERSION .
+# docker tag account-app-frontend:$VERSION gsemir/account-app-frontend:$VERSION
+# docker push gsemir/account-app-frontend:$VERSION
+docker buildx create --name mybuilder --use
+docker buildx build --platform linux/amd64,linux/arm64 -t gsemir/account-app-frontend:$VERSION . --push
