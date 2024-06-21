6ff11400ccc7b1bebf362419e231796d5833edc7
gsemir0418 <845217811@qq.com>
2024-06-21 20:35:38 +0800

    ui: item list

--- src/pages/tag/components/item-list.tsx
+++ src/pages/tag/components/item-list.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from 'react'
 import dayjs from 'dayjs'
+import { Link } from 'react-router-dom'
 import type { Item } from '@/types/model'
 import { useHomeStore } from '@/stores/useHomeStore'
 
@@ -19,12 +20,22 @@ export const ItemList: React.FC<Props> = ({ items }) => {
   }, [yearMonth, items])
 
   return (
-    <div>
+    <div className="flex-1 w-full">
+      {!filteredData || filteredData.length === 0 && (
+        <div className="flex flex-col items-center justify-center">
+          <span className="text-xl text-zinc-400">
+            暂无记录，去
+            <Link to="/items/new" className="text-teal-500">记一笔</Link>
+          </span>
+        </div>
+      )}
       {filteredData.map(item => (
-        <div key={item.id}>
-          {item.happenedAt}
-          {' '}
-          {item.amount / 1000}
+        <div key={item.id} className="flex flex-row items-center justify-between p-2 border-b">
+          <span className={`text-xl ${item.kind === 'income' ? 'text-green-500' : 'text-red-500'}`}>
+            {item.kind === 'income' ? '+' : '-'}
+            {item.amount / 1000}
+          </span>
+          <span className="text-sm text-zinc-400">{dayjs(item.happenedAt).format('YYYY-MM-DD HH:mm')}</span>
         </div>
       ))}
     </div>
