b02672fe563aa4150c11923fce7fc091073bba24
gsemir0418 <845217811@qq.com>
2024-06-23 19:59:51 +0800

    ci: push script supports pushing to hubor or docker hub

--- push.sh
+++ push.sh
@@ -7,8 +7,21 @@ if [ -z "$VERSION" ]; then
     exit 1
 fi
 
+TARGET=$2
+if [ "$TARGET" = "dh" ]; then
+  TAG=gsemir/account-app-frontend:$VERSION
+elif [ "$TARGET" = "h" ]; then
+  TAG=192.168.31.117:70/account-app/account-app-frontend:$VERSION
+else
+  echo "未知目标。请指定 'dockerhub' 或 'harbor'。"
+  exit 1
+fi
+
 # docker build -t account-app-frontend:$VERSION .
 # docker tag account-app-frontend:$VERSION gsemir/account-app-frontend:$VERSION
 # docker push gsemir/account-app-frontend:$VERSION
-docker buildx create --name mybuilder --use
-docker buildx build --platform linux/amd64,linux/arm64 -t gsemir/account-app-frontend:$VERSION . --push
+# docker buildx create --name mybuilder --use
+# docker buildx rm mybuilder
+
+docker buildx create --name mybuilder --config ./buildkitd.toml --use
+docker buildx build --platform linux/amd64,linux/arm64 -t $TAG . --push
