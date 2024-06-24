9af39083fe80ffcd28eda8324c23821a443eb0da
gsemir0418 <845217811@qq.com>
2024-06-24 19:42:55 +0800

    feat: tab component that implements income and expense page switching based on css snap scroll and IntersectionObserver API

---
+++ src/pages/home/components/tab.tsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { TagCard } from './tag-card'
+import type { TagSummary } from '@/types/model'
+
+interface Props {
+  tabList: {
+    key: React.Key
+    label1: string
+    label2: string
+    className: string
+    value: TagSummary[]
+  }[]
+}
+
+interface TabCardProps {
+  tagList: TagSummary[]
+  id: React.Key
+}
+
+export const TabCard: React.FC<TabCardProps> = ({ tagList, id }) => {
+  return (
+    <div id={id as string} className="h-full min-w-full snap-center snap-normal text-center px-4">
+      {tagList.length === 0
+        ? (
+          <div className="text-teal-700">
+            暂无记录，去
+            <Link className="text-blue-500" to="/tags/new">创建标签</Link>
+          </div>
+        )
+        : (
+          <div className="grid grid-cols-3 gap-2">
+            {tagList.map(tag => (
+              <TagCard {...tag} key={tag.id} />
+            ))}
+          </div>
+        )}
+    </div>
+  )
+}
+
+export const Tab: React.FC<Props> = ({ tabList }) => {
+  const [activeKey, setActiveKey] = useState('expense')
+  const observer = useRef<IntersectionObserver>()
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting)
+            setActiveKey(entry.target.id)
+        })
+      },
+      { threshold: 0.5 },
+    )
+
+    tabList.forEach((tab) => {
+      observer.current?.observe(document.getElementById(tab.key as string)!)
+    })
+
+    return () => {
+      observer.current?.disconnect()
+      observer.current = undefined
+    }
+  }, [])
+
+  const handleTabClick = (key: string) => {
+    document.getElementById(tabList.find(tab => tab.key === key)?.key as string)?.scrollIntoView({ behavior: 'smooth' })
+    setActiveKey(key)
+  }
+
+  return (
+    <div className="flex-1 flex flex-col">
+      <div className="w-full flex justify-between px-8 mb-2 text-sm text-zinc-400">
+        {tabList.map(tab => (
+          <span
+            key={tab.key}
+            className={`${activeKey === tab.key ? ' border-b-2 border-teal-200 text-zinc-600' : ''}`}
+            onClick={() => handleTabClick(tab.key as string)}
+          >
+            {tab.label1}
+            <span className={tab.className}>{tab.label2}</span>
+          </span>
+        ))}
+      </div>
+      <div className="flex-1 flex w-full relative snap-x snap-mandatory overflow-auto hide-scrollbar">
+        {tabList.map(tab => (
+          <TabCard key={tab.key} id={tab.key} tagList={tab.value} />
+        ))}
+      </div>
+    </div>
+  )
+}
--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -6,6 +6,7 @@ import { TagCard } from './components/tag-card'
 import { AddButton } from './components/add-button'
 import { DatePickerDrawer } from './components/date-picker-drawer'
 import { Popover } from './components/popover'
+import { Tab } from './components/tab'
 import { useHomeStore } from '@/stores/useHomeStore'
 import type { TagSummary } from '@/types/model'
 
@@ -32,7 +33,7 @@ export const HomePage: React.FC<Props> = () => {
   const dataGroupByKind = useMemo(() => {
     const _data = data?.data.resources
     if (!_data || _data.length === 0)
-      return []
+      return { income: [], expense: [] }
     const income: TagSummary[] = []
     const expense: TagSummary[] = []
     _data.forEach((tag) => {
@@ -45,7 +46,7 @@ export const HomePage: React.FC<Props> = () => {
       income: income.reduce((sum, tag) => sum + tag.summary / 1000, 0),
       expense: expense.reduce((sum, tag) => sum + tag.summary / 1000, 0),
     })
-    return income.concat(expense)
+    return { income, expense }
   }, [data])
 
   if (error)
@@ -55,31 +56,12 @@ export const HomePage: React.FC<Props> = () => {
     <div className="h-full flex flex-col items-center">
       <div className="text-2xl text-teal-500 m-10 font-['ZiHunShiGuang']">这可能是一个记账软件？</div>
       <div className="text-teal-500 border-b-2 text-xl border-teal-200 mb-2" onClick={() => setIsDatePickerOpen(true)}>{yearMonth}</div>
-      <div className="w-full flex justify-between px-4 mb-2 text-sm text-zinc-500">
-        <span>
-          当月总支出
-          <span className="text-red-500 ml-2">{total.expense.toFixed(2)}</span>
-        </span>
-        <span>
-          当月总收入
-          <span className="text-teal-500 ml-2">{total.income.toFixed(2)}</span>
-        </span>
-      </div>
       {isLoading && 'loading...'}
-      {dataGroupByKind.length === 0
-        ? (
-          <div className="text-teal-700">
-            暂无记录，去
-            <Link className="text-blue-500" to="/tags/new">创建标签</Link>
-          </div>
-        )
-        : (
-          <div className="grid grid-cols-3 gap-2">
-            {dataGroupByKind.map(tag => (
-              <TagCard {...tag} key={tag.id} />
-            ))}
-          </div>
-        )}
+      <Tab tabList={[
+        { key: 'expense', label1: '支出', label2: total.expense.toFixed(2), className: 'text-red-500 ml-2', value: dataGroupByKind.expense },
+        { key: 'income', label1: '收入', label2: total.income.toFixed(2), className: 'text-teal-500 ml-2', value: dataGroupByKind.income },
+      ]}
+      />
       <AddButton onAddClick={handleAddButtonClick} />
       <DatePickerDrawer isOpen={isDatePickerOpen} onClose={() => setIsDatePickerOpen(false)} onChange={onSubmit} />
       <Popover show={showModal} onClose={handlePopoverClose} />
